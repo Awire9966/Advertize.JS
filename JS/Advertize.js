2715d6f32c02ae5e763fabe351410a9369427593
Andrew Cavallo <65136929+Awire9966@users.noreply.github.com>
2021-11-24 11:28:18 -0500

    Update Advertize.js

--- JS/Advertize.js
+++ JS/Advertize.js
@@ -1,1 +1,20 @@
+//Put the sites you have to advertise here.
+const sites = ["", "", ""];
+//Do not touch unless you know what your doing!
+const sitecount = sites.length;
+const one = 1;
+var int = getRandomInt(20)
 
+
+
+
+
+
+function createwindow(url)
+{
+   var Window = window.open(url, '', 'scrollbars=1,height='+screen.availHeight+',width='+screen.availWidth);
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
